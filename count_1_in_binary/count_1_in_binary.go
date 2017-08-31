c217fafe0f5fe528bf4224249f682f4381607e36
Ying Zou <xpzouying@gmail.com>
2017-08-31 09:40:07 +0800

    comments unuseful print.

--- count_1_in_binary/count_1_in_binary.go
+++ count_1_in_binary/count_1_in_binary.go
@@ -24,20 +24,15 @@ func count(num uint32) uint32 {
 
 		// if the smallest is 1
 		if num&1 == 1 {
-			println("number is: ", num, " would +1")
 			total += 1
 		}
 
 		// num = num >> 1
 		num >>= 1
-		println("after >> 1, num=", num)
 	}
 
 }
 
 func main() {
-	fmt.Println("vim-go")
-
-	// strconv.FormatInt(1023, 2)
 	fmt.Printf("1023: %b\n", 1023)
 }
