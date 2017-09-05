a2e64cc069b7e935cf3035006e64e76b432ce914
Ying Zou <xpzouying@gmail.com>
2017-09-05 15:50:08 +0800

    string_permutation: update testcase for testcase sets.
    learning from justforfunc(youtube)

--- string-permutation/string_permutation_test.go
+++ string-permutation/string_permutation_test.go
@@ -8,20 +8,42 @@ func assertEqual(t *testing.T, expect, result bool) {
 	t.Helper()
 
 	if result != expect {
-		t.Errorf("expect: %v, but result is %v", expect, result)
+		t.Fatalf("expect: %v, but result is %v", expect, result)
 	}
 }
 
 func TestPermutation1(t *testing.T) {
-	assertEqual(t, true, permutation1("abc", "cba"))
-	assertEqual(t, true, permutation1("aabc", "abac"))
+	tt := []struct {
+		name     string
+		expected bool
+		ss       [2]string
+	}{
+		{"abc vs cba is true", true, [2]string{"abc", "cba"}},
+		{"aabc vs abac is true", true, [2]string{"aabc", "abac"}},
+		{"aabc vs abcc is false", false, [2]string{"aabc", "abcc"}},
+	}
 
-	assertEqual(t, false, permutation1("aabc", "abcc"))
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			assertEqual(t, tc.expected, permutation1(tc.ss[0], tc.ss[1]))
+		})
+	}
 }
 
 func TestPermutation2(t *testing.T) {
-	assertEqual(t, true, permutation1("abc", "cba"))
-	assertEqual(t, true, permutation1("aabc", "abac"))
+	tt := []struct {
+		name     string
+		expected bool
+		ss       [2]string
+	}{
+		{"abc vs cba is true", true, [2]string{"abc", "cba"}},
+		{"aabc vs abac is true", true, [2]string{"aabc", "abac"}},
+		{"aabc vs abcc is false", false, [2]string{"aabc", "abcc"}},
+	}
 
-	assertEqual(t, false, permutation1("aabc", "abcc"))
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			assertEqual(t, tc.expected, permutation2(tc.ss[0], tc.ss[1]))
+		})
+	}
 }
